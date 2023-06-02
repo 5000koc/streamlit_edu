807ca2f9aacc784080607ea24df8c8eb5028b588
5000koc <kocran5000@gmail.com>
2023-06-02 13:01:50 +0900

    add 20230602

--- app_utils.py
+++ app_utils.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 
-# 디렉토리이름과 파일을 주면 해당 디렉토리에 파일을 저장해주는 함수 강사님의 
+# 디렉토리이름과 파일을 주면 해당 디렉토리에 파일을 저장해주는 함수 
 def save_uploaded_file(directory, file):
     # 1. 저장할 디렉토리가 있는지 확인하고 없으면 디렉토리를 먼저 만든다
     if not os.path.exists(directory):
